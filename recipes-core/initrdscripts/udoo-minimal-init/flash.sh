67bda88ba8747723e4a0391f58fca789b9039ee0
Vincent Davis Jr <vince@underview.tech>
2025-07-12 16:56:56 -0400

    udoo-minimal-init: remove grub-install
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- recipes-core/initrdscripts/udoo-minimal-init/flash.sh
+++ recipes-core/initrdscripts/udoo-minimal-init/flash.sh
@@ -7,9 +7,6 @@ efi_dir="/tmp/efi"
 data_dir="/tmp/data"
 grub_modules="normal part_msdos part_gpt multiboot"
 
-mkdir -p "${efi_dir}"
-mkdir -p "${data_dir}"
-
 if [ ! -b "${rootdev}" ]; then
 	echo "Waiting for ${rootdev}..."
 	while [ $count -ne 10 ]; do
@@ -36,61 +33,3 @@ bmaptool copy --bmap "${liveusb_mnt}/emmc-wic-udoo-bolt-emmc.rootfs.wic.bmap" \
 	echo "[x] bmaptool: copy ${liveusb_mnt}/emmc-wic-udoo-bolt-emmc.rootfs.wic.gz -> ${rootdev} failed"
 	exec sh
 }
-
-partprobe
-
-efi_boot="$(blkid --match-token PARTLABEL="efi_system" -o device)"
-grub_data="$(blkid --match-token PARTLABEL="grub_data" -o device)"
-
-mount -v "${efi_boot}" "${efi_dir}" || \
-{
-	echo "[x] mount ${efi_boot} -> ${efi_dir} failed!"
-	exec sh
-}
-
-mount -v "${grub_data}" "${data_dir}" || \
-{
-	echo "[x] mount ${grub_data} -> ${data_dir} failed!"
-	umount "${efi_dir}"
-	exec sh
-}
-
-# Install grub uefi
-"${liveusb_mnt}/grub-efi"/grub-install \
-	--target="x86_64-efi" \
-	--bootloader-id="GRUB" \
-	--no-nvram \
-	--directory="${liveusb_mnt}/grub-efi/grub-core" \
-	--efi-directory="${efi_dir}" \
-	--modules="${grub_modules}" \
-	--root-directory="${data_dir}" \
-	--no-floppy "${rootdev}" || \
-{
-	echo "[x] grub-install: Failed to install UEFI grub!"
-	umount "${efi_dir}"
-	umount "${data_dir}"
-	exec sh
-}
-
-# Install grub bios
-"${liveusb_mnt}/grub-bios"/grub-install \
-	--target="i386-pc" \
-	--directory="${liveusb_mnt}/grub-bios/grub-core" \
-	--root-directory="${data_dir}" \
-	--modules="${grub_modules}" \
-	--no-floppy "${rootdev}" || \
-{
-	echo "[x] grub-install: Failed to install BIOS grub!"
-	umount "${efi_dir}"
-	umount "${data_dir}"
-	exec sh
-}
-
-mkdir -p "${efi_dir}/EFI/BOOT"
-mkdir -p "${data_dir}/boot/grub"
-
-cp -av "${efi_dir}/EFI/GRUB/grubx64.efi" "${efi_dir}/EFI/BOOT/BOOTX64.EFI"
-cp -av "${liveusb_mnt}/image-boot-files/grub.cfg" "${data_dir}/boot/grub/grub.cfg"
-
-umount "${efi_dir}"
-umount "${data_dir}"
